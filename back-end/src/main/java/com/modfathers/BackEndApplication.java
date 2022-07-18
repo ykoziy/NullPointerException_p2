7d789c35fa692c3a4dba636275159e270854ec53
Yuriy Koziy <yukoziy@gmail.com>
2022-07-18 17:24:23 -0400

    initial commit

---
+++ back-end/src/main/java/com/modfathers/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.modfathers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
