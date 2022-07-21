f5bb6732eef3a3b068fbe8372690aa8881278226
Yuriy Koziy <yukoziy@gmail.com>
2022-07-21 09:53:02 -0400

    Fix tests, since using LocalDateTime for User

--- back-end/src/test/java/com/modfathers/service/UserServiceTest.java
+++ back-end/src/test/java/com/modfathers/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -26,64 +27,70 @@ import com.modfathers.repository.UserRepository;
 public class UserServiceTest {
 	@Mock
 	private UserRepository userRepo;
-	
+
 	private UserService userService;
-	
+
 	@BeforeEach
 	void beforeEach() {
 		userService = new UserService(userRepo);
 	}
-	
+
 	@Test
 	void newUserIsCreatedWithValidDate() {
-		User testUser = new User(12, "Bob", "Smith", "testUser", "password", "bob@example.com", "4206669999", LocalDate.now());
+		User testUser = new User(12, "Bob", "Smith", "testUser", "password", "bob@example.com", "4206669999",
+				LocalDateTime.now());
 		when(userRepo.save(any(User.class))).thenReturn(testUser);
 		User u = userService.registerUser("testUser", "password", "Bob", "Smith", "4206669999", "bob@example.com");
 		assertEquals(testUser, u);
 	}
-	
+
 	@Test
 	void cantRegistreIfEmailExists() {
-		User testUser = new User(12, "Bob", "Smith", "testUser", "password", "bob@example.com", "4206669999", LocalDate.now());
-		when(userRepo.save(any(User.class))).thenThrow(new UserAlreadyExistException("User already exists with this email"));
-		Throwable exception =  assertThrows(UserAlreadyExistException.class, () -> {
-			User u = userService.registerUser("testUser", "password", "Bob", "Smith", "4206669999", "bobs@example.com");	
+		User testUser = new User(12, "Bob", "Smith", "testUser", "password", "bob@example.com", "4206669999",
+				LocalDateTime.now());
+		when(userRepo.save(any(User.class)))
+				.thenThrow(new UserAlreadyExistException("User already exists with this email"));
+		Throwable exception = assertThrows(UserAlreadyExistException.class, () -> {
+			User u = userService.registerUser("testUser", "password", "Bob", "Smith", "4206669999", "bobs@example.com");
 		});
-		
+
 		assertEquals("User already exists with this email", exception.getMessage());
 	}
-	
-	@Test 
+
+	@Test
 	void userCanLogin() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String encoded = encoder.encode("password");
-		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999", LocalDate.now());
+		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999",
+				LocalDateTime.now());
 		when(userRepo.findByUserName(anyString())).thenReturn(Optional.of(testUser));
 		User u = userService.loginUser("testUser", "password");
 		assertEquals(testUser, u);
 	}
-	
-	@Test 
+
+	@Test
 	void userCantLoginWrongPassword() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String encoded = encoder.encode("password");
-		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999", LocalDate.now());
+		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999",
+				LocalDateTime.now());
 		when(userRepo.findByUserName(anyString())).thenReturn(Optional.of(testUser));
-		Throwable exception =  assertThrows(UserAuthenticationException.class, () -> {
+		Throwable exception = assertThrows(UserAuthenticationException.class, () -> {
 			User u = userService.loginUser("testUser", "123");
-		});		
+		});
 		assertEquals("Wrong password for username: testUser", exception.getMessage());
 	}
-	
-	@Test 
+
+	@Test
 	void userCantLoginNoSuchUsername() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String encoded = encoder.encode("password");
-		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999", LocalDate.now());
+		User testUser = new User(12, "Bob", "Smith", "testUser", encoded, "bob@example.com", "4206669999",
+				LocalDateTime.now());
 		when(userRepo.findByUserName(anyString())).thenReturn(Optional.empty());
-		Throwable exception =  assertThrows(UserAuthenticationException.class, () -> {
+		Throwable exception = assertThrows(UserAuthenticationException.class, () -> {
 			User u = userService.loginUser("testUser2", "password");
-		});		
-		assertEquals("Can't find user with such username: testUser2", exception.getMessage());		
+		});
+		assertEquals("Can't find user with such username: testUser2", exception.getMessage());
 	}
 }
