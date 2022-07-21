c30855fb159369758043877b102f2c5d5a5d2216
Yuriy Koziy <yukoziy@gmail.com>
2022-07-20 20:15:35 -0400

    fix

--- back-end/src/main/java/com/modfathers/repository/CreditCardRepository.java
+++ back-end/src/main/java/com/modfathers/repository/CreditCardRepository.java
@@ -1,12 +1,8 @@
 package com.modfathers.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.modfathers.model.CreditCard;
-import com.modfathers.model.Payment;
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, Integer> {
-	List<Payment> findByPaymentId(int payment_id);
+
 }
