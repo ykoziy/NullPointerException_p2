c3aa1b5a73627c96eed624e4182d5cda064ddbf8
Yuriy Koziy <yukoziy@gmail.com>
2022-07-23 19:51:48 -0400

    Fixed UserServiceTest

--- back-end/src/test/java/com/modfathers/service/UserServiceTest.java
+++ back-end/src/test/java/com/modfathers/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.modfathers.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,15 +11,16 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -81,13 +84,10 @@ public class UserServiceTest {
 		testUser.setPhone("4206669999");
 		testUser.setRegistrationDate(LocalDateTime.now());
 		
-		when(userRepo.save(any(User.class)))
-				.thenThrow(new UserAlreadyExistException("User already exists with this email"));
-		Throwable exception = assertThrows(UserAlreadyExistException.class, () -> {
-			userService.registerUser("testUser", "password", "Bob", "Smith", "4206669999", "bobs@example.com");
+		when(userRepo.findByEmail("bob@example.com")).thenReturn(Optional.of(testUser));
+		assertThrows(UserAlreadyExistException.class, () -> {
+			userService.registerUser("testUser", "password", "Bob", "Smith", "4206669999", "bob@example.com");
 		});
-
-		assertEquals("User already exists with this email", exception.getMessage());
 	}
 
 	@Test
@@ -153,6 +153,15 @@ public class UserServiceTest {
 		assertEquals("Can't find user with such username: testUser2", exception.getMessage());
 	}
 
+	@Test
+	void shouldNotUpdateIfUserNotFound() {
+		testUser = new User();
+		testUser.setId(1);
+		when(userRepo.findById(1)).thenReturn(Optional.empty());
+		User u = userService.updateUser(testUser);
+		assertNull(u);
+	}
+	
 	@Test
 	void shouldNotUpdateUserIfNothingChanges() {
 		LocalDateTime time = LocalDateTime.now();
@@ -280,6 +289,52 @@ public class UserServiceTest {
 		assertNotNull(testUser.getPassword());
 		assertNotNull(testUser.getId());
 	}
+	
+	@Test
+	void shouldNotUpdateEmailIfAlreadyExists() {
+		testUser = new User();
+		testUser.setId(12);
+		testUser.setFirstName("Bob");
+		testUser.setLastName("Smith");
+		testUser.setUserName("testUser");
+		testUser.setPassword("pwd");
+		testUser.setEmail("bob@example.com");
+		testUser.setPhone("4206669999");
+		testUser.setRegistrationDate(LocalDateTime.now());
+
+		newUser = new User();
+		newUser.setId(12);
+		newUser.setEmail("mimimi@example.com");
+		
+		when(userRepo.findById(12)).thenReturn(Optional.of(testUser));
+		when(userRepo.findByEmail("mimimi@example.com")).thenReturn(Optional.of(testUser));
+		assertThrows(UserAlreadyExistException.class, () -> {
+			userService.updateUser(newUser);
+		});
+	}
+	
+	@Test
+	void shouldNotUpdateEmailIfEqualsToNewOne() {
+		testUser = new User();
+		testUser.setId(12);
+		testUser.setFirstName("Bob");
+		testUser.setLastName("Smith");
+		testUser.setUserName("testUser");
+		testUser.setPassword("pwd");
+		testUser.setEmail("bob@example.com");
+		testUser.setPhone("4206669999");
+		testUser.setRegistrationDate(LocalDateTime.now());
+
+		newUser = new User();
+		newUser.setId(12);
+		newUser.setEmail("bob@example.com");
+		
+		when(userRepo.findById(12)).thenReturn(Optional.of(testUser));
+		when(userRepo.save(any(User.class))).thenReturn(testUser);
+		
+		User result  = userService.updateUser(newUser);
+		assertEquals("bob@example.com", result.getEmail());
+	}
 
 	@Test
 	void shouldOnlyUpdatePhone() {
@@ -353,16 +408,19 @@ public class UserServiceTest {
 		testUser.setEmail("bob@example.com");
 		testUser.setPhone("4206669999");
 		testUser.setRegistrationDate(LocalDateTime.now());
-		when(userRepo.findByUserName("testUser")).thenReturn(Optional.of(testUser));
-		User u = userService.getByUsername("testUser");
+		when(userRepo.findById(12)).thenReturn(Optional.of(testUser));
+		User u = userService.findById(12);
 		assertEquals("bob@example.com", u.getEmail());
 	}
-
+	
 	@Test
 	void ShouldGetAllUsers() {
 		testUser2 = new User();
+		testUser2.setUserName("user2");
 		testUser = new User();
+		testUser.setUserName("user");
 		newUser = new User();
+		testUser2.setUserName("newUser");
 		
 		List<User> uList = new LinkedList<User>();
 		
@@ -371,7 +429,24 @@ public class UserServiceTest {
 		uList.add(newUser);
 		
 		when(userRepo.findAll()).thenReturn(uList);
-		List<User> resultList = userRepo.findAll();
-		assertEquals(3, resultList.size());
+		Set<User> resultSet = userService.getUsers();
+		assertEquals(3, resultSet.size());
+	}
+	
+	@Test
+	void shouldNotDeleteIfNouserFound() {
+		assertFalse(userService.delete(100));
+	}
+	
+	@Test
+	void shouldDeleteUser() {
+		when(userRepo.existsById(100)).thenReturn(true);
+		assertTrue(userService.delete(100));
+	}
+	
+	@Test
+	void shouldNotGetUserForInvalidId() {
+		User u = userService.findById(0);
+		assertNull(u);
 	}
 }
