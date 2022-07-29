b484e4b6b5ecb401c925e4222ce6da25a69ddc90
Yuriy Koziy <yukoziy@gmail.com>
2022-07-29 00:52:55 -0400

    fixed register script

--- front-end/js/register.js
+++ front-end/js/register.js
@@ -141,7 +141,7 @@ const STATES = [
         console.log(result);
         return;
       } else {
-        CookieManager.setCookie(result);
+        CookieManager.setCookie(res);
         location.href = '/index.html';
       }
     }
